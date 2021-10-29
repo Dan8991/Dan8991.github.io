e01d5236c338a607160c99c01169aed2ce997e5f
Daniele Mari <daniseas98@gmail.com>
2021-10-29 06:27:26 +0100

    [Add]animations for all paragraphs

--- script.js
+++ script.js
@@ -10,17 +10,30 @@ const observer_right = new IntersectionObserver(entries => {
     });
   });
   
-  const observer_left = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-      const project_block = entry.target.querySelector('.animated_left');
-  
-      if (entry.isIntersecting) {
-        project_block.classList.add('project-animation-left');
-        return;
-      }
-  
-    });
-  });  
+const observer_left = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    const project_block = entry.target.querySelector('.animated_left');
+
+    if (entry.isIntersecting) {
+      project_block.classList.add('project-animation-left');
+      return;
+    }
+
+  });
+});  
+
+const observer_bottom = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    const project_block = entry.target.querySelector('.animated_bottom');
+
+    if (entry.isIntersecting) {
+      project_block.classList.add('project-animation-bottom');
+      return;
+    }
+
+  });
+});  
+
 
 selected = document.querySelectorAll('.projects_right');
 selected.forEach(s => {
@@ -30,4 +43,9 @@ selected.forEach(s => {
 selected = document.querySelectorAll('.projects_left');
 selected.forEach(s => {
   observer_left.observe(s);
+});
+
+selected = document.querySelectorAll('.projects_bottom');
+selected.forEach(s => {
+  observer_bottom.observe(s);
 });
